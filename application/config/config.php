21ddf6e1c0cfc8125b6a243a7d906f535fb5f6b2
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-04-22 16:10:07 +0800

    T

--- application/config/config.php
+++ application/config/config.php
@@ -398,7 +398,10 @@ $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+// $config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
+$config['sess_save_path'] = 'ci_sessions';
+$config['sess_table_name'] = 'ci_sessions';
 
 
 // $config['sess_driver'] = 'files';
