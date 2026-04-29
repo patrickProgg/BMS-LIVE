237ef79b0ddc43c6c74989cb82fe3a7793060230
patrickProgg <patrickhenrybersaluna115@gmail.com>
2026-04-29 14:57:54 +0800

    T

--- application/views/monitoring.php
+++ application/views/monitoring.php
@@ -1567,8 +1567,8 @@
                     globalFishStartDate = startDate;
                     globalFishEndDate = dueDate;
 
-                    // const date_now = new Date().toISOString().split('T')[0];
-                    const date_now = "2026-06-01";
+                    const date_now = new Date().toISOString().split('T')[0];
+                    // const date_now = "2026-06-01";
 
                     $('.fish-btn').html(dateRange);
 
@@ -1743,8 +1743,8 @@
 
                     globalRiceStartDate = startDate;
                     globalRiceEndDate = dueDate;
-                    // const date_now = new Date().toISOString().split('T')[0];
-                    const date_now = "2026-06-01";
+                    const date_now = new Date().toISOString().split('T')[0];
+                    // const date_now = "2026-06-01";
 
                     $('.rice-btn').html(dateRange);
 
